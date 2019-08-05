7e3e58842d70336a721806456833a11adc630f38
Jinyue Chen <jinyue.chen@kuang-chi.org>
2019-08-05 18:48:30 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {Button, Input, Icon} from 'antd';
+// import 'antd/es/button/style';
 
+import {hot} from 'react-hot-loader/root';
+import './App.css';
+import ComA from './comA';
 const App: React.FC = () => {
+  let [count, setCount] = useState(12)
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,14 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ComA></ComA>
+        <Icon type="import" />
+        <Icon type="copyright" />
+        <h1>{count}</h1>
+        <button className="ant-message" >add</button>
+        <Button type="primary" onClick={() => setCount(count+1)}>Button</Button>
+        <Button type="primary">Primary</Button>
+        <Input></Input>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,4 +36,4 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
+export default hot(App);
---
+++ src/comA.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ComA: React.FC = () => {
+  return (
+    <div style={{color: 'blue'}}>
+      fdsafdsafdsaf
+    </div>
+  )
+}
+
+export default ComA
